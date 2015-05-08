09fe99ca4a8f70847515a4f42b4899400e4b8912
Cameron Prebble <cameron.prebble@abletech.co.nz>
2015-05-08 11:38:34 +0200

    add github link to footer

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :project do
     name { Faker::Commerce.product_name }
+    role_title { Faker::Name.title }
     category { Faker::Lorem.word }
     client { Faker::Company.name }
     url { Faker::Internet.url }
